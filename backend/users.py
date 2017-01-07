47e6275e0cdff0fdfad55f48cc88aefca7b355e3
chenyouyee <youyee@gmail.com>
2017-01-07 16:02:31 -0500

    added Student class

--- backend/users.py
+++ backend/users.py
@@ -42,3 +42,62 @@ class User:
         Returns the email of the user
         """
         return self._email
+    
+    
+class Student():
+    def __init__(self):
+        
+        # set of courses that the Student is enrolled in, as Course objects 
+        self._courses = set([])
+        
+    def enroll_course(self, Course):
+        '''(Student, Course) -> NoneType
+        Enrolls the student into the course specified.
+        
+        '''
+        
+        # if the Student is already enrolled in the course,
+        # don't let them enroll again
+        if Course not in self._courses:
+            
+            # add course to Student's enrolled courses
+            self._courses.update(Course)
+            
+            # add Student into that Course
+            Course.add_student(self)
+            
+    def drop_course(self, Course):
+        '''(Student, Course) -> NoneType
+        Unenrolls the student from the course specified.
+        
+        '''
+        # Students can only drop courses they are enrolled in
+        if Course in self._courses:
+            
+            # remove the Course from Student's list of enrolled courses
+            self._courses.remove(Course)
+            
+            # remove Student from that Course
+            Course.remove_student(self)
+            
+    def get_courses():
+        '''(None) -> list of Courses
+        Returns the courses the Student is enrolled in as a list.
+        
+        '''
+        course_list = []
+        
+        for Course in self._courses:
+            course_list.append(Course)
+            
+        return course_list
+    
+    def get_course(self, Course):
+        '''(Student, Course) -> set of Events    
+        Returns all the Events (ie. office hours) for the specified Course.
+        
+        '''
+        
+
+        
+    
