bd20d7074d6e93e77e7e3dff4a0da5d1764ac8f9
6u55ki11 <alexwaltercann@gmail.com>
2017-01-07 20:17:12 -0500

    Add files via upload

--- backend/Course.py
+++ backend/Course.py
@@ -3,80 +3,80 @@ import time
 from users import *
 DAYS = ['Monday','Tuesday','Wednesday','Thursday','Friday']
 
-class Course:
+class Course(object):
     '''
     A class to represent a course including the students and timetable
     '''
-    def __init__(self, admin, tutor_times, name) -> None:
-
+    def __init__(self: Course, admin: Admin ,tutor_times: TutorTimes,name: str) -> None:
+        
         # create the private variables
         self._admins = set()
         self._students = set()
         self._admins.add(admin)
         self._name = name
-
-        self._TutorEvents = set()
+        
+        self._tutor_events = set()
         self._tutor_times = tutor_times
-
-        # initialize
-        change_Event = Event()
-        time_Event = Event()
-
+        
+        # initialize 
+        self._change_Event = Event()
+        self._time_Event = Event()
+        
         self._time_monitor = TimeMonitor(self,time_event)
         self._change_monitor = Changemonitor(self,change_event)
-
+        
         # start the threads
         self._time_monitor.start()
-        self._change_monitor.start()
-
-    def add_student(self,student):
+        self._change_monitor.start()        
+        
+    def add_student(self: Course,student: Student) -> None:
         self._students.add(student)
-
-    def remove_student(self,student):
+    
+    def remove_student(self: Course,student: Student) -> None:
         self._students.difference_update(set(student))
-
-    def add_admin(self,admin):
+        
+    def add_admin(self: Course,admin: Admin):
         self._admins.add(student)
 
-    def remove_admin(self,admin):
+    def remove_admin(self: Course,admin: Admin):
         self._admins.difference_update(set(admin))
 
-    def add_TutorEvent(self,day,TutorEvent):
+    def add_TutorEvent(self: Course, event):
         self._change_event.set()
-        self._TutorEvents.add(TutorEvent)
+        self._TutorEvents.add(event)
 
-    def remove_TutorEvent(self,day,TutorEvent):
-        self._TutorEvents.difference_update(set(TutorEvent))
+    def remove_TutorEvent(self, event):
+        self._TutorEvents.difference_update(set(event))
         self._change_event.set()
 
     def notify(self,message):
         self._tutor_times.notify(students,message)
-
+    
     def get_TutorEvents():
-        return self._TutorEvents()
-
+        return self._tutor_events
+    
     def get_name():
         return self._name
 
-
+    
 class TutorEvent:
-
+    
     def __init__(self, start_time, end_time, room, name):
         self._start = start_time
         self._end = end_time
         self._room = room
         self._name = name
         self._already_notified = False
-
+        
     def get_name(self):
         return self._name
-
+    
     def get_room(self):
         return self._room
-
+    
     def edit_start(self,new_time):
         self._start = new_time
-
+        
     def edit_end(self,new_time):
         self._end = new_time
 
@@ -86,7 +86,7 @@ class TutorEvent:
         '''
         curr_time = int(round(time.time() * 1000))
         output = False
-        if(self._start <= curr_time <= end_time
+        if(self._start <= curr_time <= end_time 
            and not self._currently_notified):
             output = True
             self._currently_notified = True
@@ -106,7 +106,7 @@ class TimeMonitor(Thread):
         Thread.__init__(self)
         self._course = course
         self._event = event
-
+        
     def run(self):
         while True:
             # 45 seconds in millis
@@ -122,9 +122,9 @@ class TimeMonitor(Thread):
                     if(longest_wait < curr_length):
                         longest_wait = curr_length
             self._event.clear()
-            self._event.wait(longest_wait/1000)
-
-
+            self._event.wait(longest_wait/1000)    
+            
+                    
 class ChangeMonitor(Thread):
     '''
     monitors wether an update has been made to the TutorEvents
